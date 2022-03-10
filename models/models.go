a135034985d14082e167aefb84bf9c147a8cd983
Emmrys-Jay <jonathanemma121@gmail.com>
2022-03-10 06:50:43 +0100

    Complete basic db functins for the user info table
    I wrote the queries to INSERT and QUERY the db for user information during signup and signin respectively.

--- models/models.go
+++ models/models.go
@@ -2,19 +2,37 @@ package models
 
 import (
 	"database/sql"
-	"mime/multipart"
+	"fmt"
+	"net/http"
 )
 
 var db *sql.DB
+var err error
 
 func ConnectDB() {
+	db, err = sql.Open("mysql", "admin:my_photo_blog-emmrys@tcp(database-2.cpyezbxep7pq.us-east-2.rds.amazonaws.com)/myphotoblog?charset=utf8")
+	if err != nil {
+		panic(err)
+	}
 
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("you connected to database successfully")
 }
 
-func Adduser(email, name, description string) {
-
+func Adduser(w http.ResponseWriter, uname, email, psword string) error {
+	stmt := fmt.Sprintf("INSERT INTO myphotoblog.userspb VALUES('%s', '%s', '%s')", uname, email, psword)
+	_, err = db.Exec(stmt)
+	return err
 }
 
-func AddPicture(pic *multipart.File, uname, pname, desc string) {
+func AddPicture(uname, pname, desc, photop string) error {
+}
 
+func GetUser(uname string) *sql.Row {
+	stmt := fmt.Sprintf("SELECT uname, psword FROM myphotoblog.userspb WHERE uname = '%s'", uname)
+	row := db.QueryRow(stmt)
+	return row
 }
