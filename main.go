96a2755efa2f1455ce79ac0ffc3e543ec4c910c5
Emmrys-Jay <jonathanemma121@gmail.com>
2022-03-12 08:58:34 +0100

    Final Touches
    I added functionality to serve all picture files on the server using http.StripPrefix and http.FileServer. I finally understood that the StripPrefix function removes the prefix from the tag where the prefix is used to get a file in the .gohtml template file.

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"Golang/my-photo-blog/controllers"
+	"Golang/my-photo-blog/models"
 	"net/http"
 )
 
 func main() {
+	models.ConnectDB()
 	m := controllers.GetMuxVar()
-	http.HandleFunc("/", m.Index)
+	http.Handle("/", http.StripPrefix("/views/pics", http.FileServer(http.Dir("./views/pics"))))
+	http.HandleFunc("/view", m.Index)
 	http.HandleFunc("/signin", m.Signin)
 	http.HandleFunc("/signup", m.Signup)
-	http.HandleFunc("/add", m.Add)
+	http.HandleFunc("/add", m.Addpic)
+	http.HandleFunc("/logout", m.Signout)
 	http.ListenAndServe(":8080", nil)
 }
