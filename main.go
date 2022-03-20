ca211e6a72fd2b1057a13e86892cf3ca6034ea01
Emmrys-Jay <jonathanemma121@gmail.com>
2022-03-20 18:10:56 +0100

    Added functionality
    I changed the name of the 'index' function to 'ReadPics' and added handlers and paths for the update and delete feautures.

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ func main() {
 	models.ConnectDB()
 	m := controllers.GetMuxVar()
 	http.Handle("/", http.StripPrefix("/views/pics", http.FileServer(http.Dir("./views/pics"))))
-	http.HandleFunc("/view", m.Index)
+	http.HandleFunc("/view", m.ReadPics)
 	http.HandleFunc("/signin", m.Signin)
 	http.HandleFunc("/signup", m.Signup)
 	http.HandleFunc("/add", m.Addpic)
 	http.HandleFunc("/logout", m.Signout)
+	http.HandleFunc("/update", m.UpdatePic)
+	http.HandleFunc("/delete", m.DeletePic)
 	http.ListenAndServe(":8080", nil)
 }
