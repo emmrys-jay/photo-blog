d8f08cdd92496e354bbd10dc41dbfb0985b6665c
Emmrys-Jay <jonathanemma121@gmail.com>
2022-06-06 10:53:21 +0100

    Final commit

--- models/config.go
+++ models/config.go
@@ -8,11 +8,15 @@ import (
 )
 
 func ConnectDB() {
+
+	// Connect ot remote database
+	// Edit details when connecting to your PostgreSQL server instance
 	db, err = sql.Open("postgres", "user=postgres dbname=myphotoblog password=my_photo_blog-emmrys host=database-1.cpyezbxep7pq.us-east-2.rds.amazonaws.com sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
 
+	// Ping database to see if connection has been successfully established
 	err = db.Ping()
 	if err != nil {
 		panic(err)
