a7506a55cc208790790a97cb16a34bbfd14bce20
Emmrys-Jay <jonathanemma121@gmail.com>
2022-08-08 05:51:54 +0100

    Added new host for db container

--- models/config.go
+++ models/config.go
@@ -9,9 +9,9 @@ import (
 
 func ConnectDB() {
 
-	// Connect ot remote database
+	// Connect to remote database
 	// Edit details when connecting to your PostgreSQL server instance
-	db, err = sql.Open("postgres", "user=postgres dbname=myphotoblog password=my_photo_blog-emmrys host=database-1.cpyezbxep7pq.us-east-2.rds.amazonaws.com sslmode=disable")
+	db, err = sql.Open("postgres", "user=postgres dbname=myphotoblog password=password host=my-photo-blog_postgres-server_1 sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
@@ -21,5 +21,37 @@ func ConnectDB() {
 	if err != nil {
 		panic(err)
 	}
+
+	err = CreateDatabase(db)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("you connected to database successfully")
+}
+
+func CreateDatabase(db *sql.DB) error {
+	userspbQuery := `CREATE TABLE userspb (
+						uname VARCHAR PRIMARY KEY NOT NULL,
+						email VARCHAR UNIQUE NOT NULL,
+						psword VARCHAR
+					);`
+	_, err := db.Exec(userspbQuery)
+	if err != nil {
+		return err
+	}
+
+	photobQuery := `CREATE TABLE photob (
+						id BIGSERIAL,
+						uname VARCHAR,
+						ptitle VARCHAR,
+						photo VARCHAR NOT NULL,
+						descp VARCHAR DEFAULT NULL,
+						PRIMARY KEY(id, uname),
+						FOREIGN KEY(uname) REFERENCES userspb(uname) ON DELETE CASCADE
+					);`
+
+	_, err = db.Exec(photobQuery)
+
+	return err
 }
